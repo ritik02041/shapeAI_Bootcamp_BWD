3d2870c2678013668b7b5554a1d1d53b4201c007
ritik02041 <69710001+ritik02041@users.noreply.github.com>
2021-06-27 21:30:36 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note(){
+return(
+   <div className="note">
+     <h1>python and java are my favourite subject</h1>
+     <p>Python is an interpreted high-level general-purpose programming language.
+     Java is a high-level, class-based, object-oriented programming language.</p>
+  </div>
+
+);
+}
+export default Note;
